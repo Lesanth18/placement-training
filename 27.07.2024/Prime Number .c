dc3cf594449934bfb238614b116e33c62744aa80
Lesanth18 <154415459+Lesanth18@users.noreply.github.com>
2024-07-27 17:15:41 +0530

    Prime Number .c

---
+++ 27.07.2024/Prime Number .c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void checkPrime(int N)
+{
+    int flag=1;
+    for (int i=2; i<=N/2;i++) {
+        if (N%i==0) {
+            flag=0;
+            break;
+        }
+    }
+if (flag) {
+        printf("The number %d is a Prime Number\n", N);
+    }
+    else {
+        printf("The number %d is not a Prime Number\n", N);
+    }
+
+    return;
+}
+int main()
+{
+    int N=546;
+
+    checkPrime(N);
+
+    return 0;
+}
