63454a731b4065b8d6bf2e59a5bd4f5b05d49040
Lesanth18 <154415459+Lesanth18@users.noreply.github.com>
2024-07-29 19:27:23 +0530

    Octal,Hexadecimal.py

---
+++ 29.07.2024/Octal,Hexadecimal.py
@@ -0,0 +1,7 @@
+def decimal_into_binary(decimal_1):  
+    decimal=int(decimal_1)  
+    
+    print ("The given decimal number",decimal,"in Binary number is: ",bin(decimal))  
+def decimal_into_octal(decimal_1):  
+    decimal=int(decimal_1)  
+print ("The given decimal number",decimal,"in Octal number is: ",oct(decima
